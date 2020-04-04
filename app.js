5d2b2b9d85ced761f689047b64708017cca740c4
Ben Flippen <benflippen@Bens-MacBook-Pro-2.local>
2020-04-03 20:18:55 -0700

    made an if statement for the calculateBudget function to only do it if the total is > 0

--- app.js
+++ app.js
@@ -68,7 +68,12 @@ let budgetController = (() => {
             // calculate the budget: income - expenses
             data.budget = data.totals.inc - data.totals.exp;
             // calculate the percentage of income that we spent
-            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+
         },
 
         getBudget: () => {
@@ -195,9 +200,8 @@ let controller = ((budgetCtrl, UICtrl) => {
         updateBudget();
         }
 
-
-
         // 6. Display the budget on the UI
+
     };
 
     return {
